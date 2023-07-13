a70a70e103772e8d93f278d2ab4ccc0444ec2e09
marcocampisi <marco.campisi@outlook.it>
2023-07-13 15:35:14 +0200

    Add header structure

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,7 +10,25 @@ export default {
 
 <template>
   <div class="headerContainer">
-    
+    <div class="logoContainer">
+      <img src="https://picsum.photos/id/1060/100/100" alt="">
+    </div>
+    <div class="navContainer">
+      <nav>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </div>
   </div>
 </template>
 
