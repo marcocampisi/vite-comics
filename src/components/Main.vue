2240dbfab72b40a13af9b10003968f4f3d39d12a
marcocampisi <marco.campisi@outlook.it>
2023-07-13 15:50:37 +0200

    Add main top mid containers

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -9,7 +9,15 @@ export default {
 </script>
 
 <template>
-  
+  <div class="mainContainer">
+    <div class="topContent">
+      <h2> Content goes here </h2>
+    </div>
+    <div class="midContent"></div>
+    <div class="botContent">
+
+    </div>
+  </div>
 </template>
 
 <style scoped>
