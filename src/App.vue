c3889caa2a92f9e13be0106bb9ec88a4157431a3
marcocampisi <marco.campisi@outlook.it>
2023-07-13 14:29:21 +0200

    Import components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
-<script setup>
+<script>
+import Header from './components/Header.vue';
+import Main from './components/Main.vue';
+import Footer from './components/Footer.vue';
 
+export default {
+    data() {
+
+    },
+    components: {
+        Header,
+        Main,
+        Footer
+    }
+}
 </script>
 
 <template>
-  
+    <div class="app">
+        <Header />
+        <Main />
+        <Footer />
+    </div>
 </template>
 
 <style scoped>
