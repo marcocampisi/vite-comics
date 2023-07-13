693e8be06cac6effa3a0b7499256c5e87a0d3976
marcocampisi <marco.campisi@outlook.it>
2023-07-13 16:01:21 +0200

    Add bot main container

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -15,7 +15,45 @@ export default {
     </div>
     <div class="midContent"></div>
     <div class="botContent">
-
+      <div class="linksCol">
+        <h3>DC COMICS</h3>
+        <p>Characters</p>
+        <p>Comics</p>
+        <p>Movies</p>
+        <p>TV</p>
+        <p>Games</p>
+        <p>Videos</p>
+        <p>News</p>
+        <h3>SHOP</h3>
+        <p>Shop DC</p>
+        <p>Shop DC Collectibles</p>
+      </div>
+      <div class="linksCol">
+        <h3>DC COMICS</h3>
+        <p>Characters</p>
+        <p>Comics</p>
+        <p>Movies</p>
+        <p>TV</p>
+        <p>Games</p>
+        <p>Videos</p>
+        <p>News</p>
+        <h3>SHOP</h3>
+        <p>Shop DC</p>
+        <p>Shop DC Collectibles</p>
+      </div>
+      <div class="linksCol">
+        <h3>DC COMICS</h3>
+        <p>Characters</p>
+        <p>Comics</p>
+        <p>Movies</p>
+        <p>TV</p>
+        <p>Games</p>
+        <p>Videos</p>
+        <p>News</p>
+        <h3>SHOP</h3>
+        <p>Shop DC</p>
+        <p>Shop DC Collectibles</p>
+      </div>
     </div>
   </div>
 </template>
