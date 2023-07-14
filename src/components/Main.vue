81f4f0bcb739c7b42dcf64959161d6703ab48428
marcocampisi <marco.campisi@outlook.it>
2023-07-14 14:28:44 +0200

    Add bot content style

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -15,6 +15,7 @@ export default {
     </div>
     <div class="midContent"></div>
     <div class="botContent">
+      <div class="dc-logo"></div>
       <div class="linksCol">
         <h3>DC COMICS</h3>
         <p>Characters</p>
@@ -29,30 +30,26 @@ export default {
         <p>Shop DC Collectibles</p>
       </div>
       <div class="linksCol">
-        <h3>DC COMICS</h3>
-        <p>Characters</p>
-        <p>Comics</p>
-        <p>Movies</p>
-        <p>TV</p>
-        <p>Games</p>
-        <p>Videos</p>
-        <p>News</p>
-        <h3>SHOP</h3>
-        <p>Shop DC</p>
-        <p>Shop DC Collectibles</p>
+        <h3>DC</h3>
+        <p>Terms Of Use</p>
+        <p>Privacy policy (new)</p>
+        <p>Ad Choices</p>
+        <p>Advertising</p>
+        <p>Jobs</p>
+        <p>Subscriptions</p>
+        <p>Talent Workshops</p>
+        <p>CPSC Certificates</p>
+        <p>Ratings</p>
+        <p>Shop Help</p>
+        <p>Contact Us</p>
       </div>
       <div class="linksCol">
-        <h3>DC COMICS</h3>
-        <p>Characters</p>
-        <p>Comics</p>
-        <p>Movies</p>
-        <p>TV</p>
-        <p>Games</p>
-        <p>Videos</p>
-        <p>News</p>
-        <h3>SHOP</h3>
-        <p>Shop DC</p>
-        <p>Shop DC Collectibles</p>
+        <h3>SITES</h3>
+        <p>DC</p>
+        <p>MAD Magazine</p>
+        <p>DC Kids</p>
+        <p>DC Universe</p>
+        <p>DC Power Visa</p>
       </div>
     </div>
   </div>
