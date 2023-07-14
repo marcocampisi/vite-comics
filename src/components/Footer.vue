2551a2a130aff8434c767ca311c2d74d9da05184
marcocampisi <marco.campisi@outlook.it>
2023-07-14 14:55:17 +0200

    Add footer style

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -10,15 +10,17 @@ export default {
 
 <template>
 	<div class="footerContainer">
-		<div class="signUp">Button</div>
+		<div class="signUp">
+			<button>SIGN-UP NOW!</button>
+		</div>
 		<div class="socials">
 			<h2>FOLLOW US</h2>
 			<div class="socialsLogos">
-				<img src="https://picsum.photos/id/1060/40/40" alt="">
-				<img src="https://picsum.photos/id/1060/40/40" alt="">
-				<img src="https://picsum.photos/id/1060/40/40" alt="">
-				<img src="https://picsum.photos/id/1060/40/40" alt="">
-				<img src="https://picsum.photos/id/1060/40/40" alt="">
+				<img src="../assets/img/footer-facebook.png" alt="">
+				<img src="../assets/img/footer-twitter.png" alt="">
+				<img src="../assets/img/footer-youtube.png" alt="">
+				<img src="../assets/img/footer-pinterest.png" alt="">
+				<img src="../assets/img/footer-periscope.png" alt="">
 			</div>
 		</div>
 	</div>
