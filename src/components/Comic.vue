03752eb17f36b20becfc3930c56b90b065bff5de
marcocampisi <marco.campisi@outlook.it>
2023-07-14 17:34:54 +0200

    Add comics container

---
+++ src/components/Comic.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: "Comic",
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        comicThumb: String,
+        comicPrice: String,
+        comicSeries: String,
+        comicType: String
+    }
+}
+</script>
+
+<template>
+    <div class="comic">
+        <div class="thumbContainer">
+            <img :src="comicThumb" :alt="comicSeries">
+        </div>
+        <h3>{{ comicSeries }}</h3>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
